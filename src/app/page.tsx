ab3ff95b736d05a18010900d0eaa76d80fd17e7b
Maus-landkreuzer <u0699522@gmail.com>
2025-09-12 17:06:59 +0530

    archi diag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-8 row-start-2 items-center w-full mx-auto">
         <QuestionPaper />
-        {/* <MarkingScheme />
-        <AnswerSheetTable /> */}
+        <MarkingScheme />
+        <AnswerSheetTable />
         <PromptRunner/>
         <ImageOcr/>
       </main>
